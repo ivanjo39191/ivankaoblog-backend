e5eac2ac94d258b95c021eaaf1ecc7f2c02e7a5a
ivanjo39191 <ivanjo39191@gmail.com>
2021-07-04 06:09:27 +0000

    feat: 新增文章次分類、文章標籤模型 說明: 1. 新增文章次分類、文章標籤 models
    調整項目:
    1. src/blog/models/blog.py 添加 BlogSubtype, BlogTag
    2. src/blog/models/__init__.py 添加 BlogSubtype, BlogTag

--- src/blog/models/__init__.py
+++ src/blog/models/__init__.py
@@ -1,6 +1,8 @@
-from .blog import Blog, BlogType
+from .blog import Blog, BlogType, BlogSubtype, BlogTag
 
 __all__ = (
     'Blog',
     'BlogType',
+    'BlogSubtype',
+    'BlogTag'
 )
--- src/blog/models/blog.py
+++ src/blog/models/blog.py
@@ -5,7 +5,7 @@ from ckeditor_uploader.fields import RichTextUploadingField  #導入富文本套
 
 from core import helpers
 
-__all__ = ('Blog', 'BlogType')
+__all__ = ('Blog', 'BlogType', 'BlogSubtype', 'BlogTag')
 
 
 # Create your models here.
@@ -15,14 +15,39 @@ class BlogType(TimeStampedModel):
     def __str__(self):
         return self.type_name
 
+    class Meta:
+        ordering = ['type_name']
+
+
+class BlogSubtype(TimeStampedModel):
+    type = models.ForeignKey(BlogType, on_delete=models.CASCADE, null=True, related_name='subtype_set')
+    subtype_name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.type} / {self.subtype_name}'
+
+    class Meta:
+        ordering = ['type']
+
+
+class BlogTag(TimeStampedModel):
+    tag_name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.tag_name
+
+    class Meta:
+        ordering = ['tag_name']
+
 
 class Blog(TimeStampedModel):
 
     title = models.CharField(max_length=200, null=True)
     content = RichTextUploadingField()  #改為RichTextField
-    type = models.ForeignKey(BlogType, on_delete=models.CASCADE)
+    types = models.ManyToManyField(BlogType, blank=True, related_name='blog_set')
+    subtypes = models.ManyToManyField(BlogSubtype, blank=True, related_name='blog_set')
+    tags = models.ManyToManyField(BlogTag, blank=True, related_name='blog_set')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    time = models.CharField(max_length=200, null=True)
     banner = models.FileField("主圖片", null=True, blank=True, upload_to=helpers.upload_handle)
 
     def __str__(self):
